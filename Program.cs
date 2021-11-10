411f09fdfa1124ba16c42a9660cefabab213ef32
Lavanya9493 <lavanyajonnalagadda123@gmail.com>
2021-11-10 18:15:10 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    public abstract class Shape
+    {
+        public string Color{get;set;}
+        public int Thickness{get;set;}
+        public abstract void Draw();
+    }
+    public class Line:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draws Line");
+        }
+    }
+    public class Rectangle:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draws Rectangle");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Shape shape1=new Line{Color="red",Thickness=1};
+            Shape shape2=new Rectangle {Color="red",Thickness=2};
+            shape1.Draw();
+            shape2.Draw();
+            Console.WriteLine(shape1);
+        }
+    }
+}
